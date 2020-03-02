765aaefa7985fdc585b9c898f3b97f6c1281ff9a
Habold <haboldmels@gmail.com>
2020-03-02 12:06:43 +0100

    first try at 1.B

--- BigAss1.py
+++ BigAss1.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 #data = pd.read_excel (r'C:\Users\Thierry\Documents\Studie\TU Delft Applied Physics\CS4195 Modeling and Data Analysis in Complex Networks\Assignment1\manufacturing_emails_temporal_network.xlsx')
 data = pd.read_excel (r'C:\Users\rixtb\Documents\Master\Data analysis\Datasets\manufacturing_emails_temporal_network.xlsx')
 #data = pd.read_excel (r'C:\Users\rixtb\Documents\Master\Data analysis\Datasets\oefenset.xlsx')
-#%%
+
+#%% A
 Nnodes = np.max([data['node1'].max(), data['node2'].max()])
 G = data.drop(['timestamp'],axis=1)
 B = G.drop_duplicates()
@@ -47,4 +48,27 @@ print(hopcount_max)
 #print(A)
 eigenvalueA= g.eigen_adjacency()
 print(eigenvalueA)
+Q = g.laplacian(weights=None, normalized=True)
+print(min(i for i in Q if i > 0))  #first non-zero eigenvalue laplacian
 
+#%% B
+tmax = data('timestamp').max()
+s = np.zeros([Nnodes, tmax+1, Nnodes], dtype = float)
+Mat = np.zeros([Nnodes, Nnodes, tmax])
+
+for t in range(tmax): #generating the adjacency matrix
+    for i in range(Nnodes):
+        for j in range(i):
+            if (data[data.timestamp.isin(t)][:,0]==i  and data[data.timestamp.isin(t)][:,0]==j) or (
+            data[data.timestamp.isin(t)][:,0]==j  and data[data.timestamp.isin(t)][:,0]==i)
+            Mat[i,j,t]=1
+
+for k in range(Nnodes): #diffeerent infection seeds
+    s[k,0,k]=1 #first infected node
+    for t in range(tmax):
+        s[:,t+1,k] = np.inner(Mat,s[:,t,k]) + s[:,t,k]   #needs to be normalized
+               
+#s becomes a matrix containing:
+    #[:,:,k] the different infection seeds
+    #[:,t,:] the different times 
+    #[n,:,:] the different particles
