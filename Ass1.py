930707f2c085abc588c2c2e97e350816eae43317
Ccorbons <ccorbons@gmail.com>
2020-03-03 18:19:24 +0100

    assignment 1A T+C, added final calculations and plots

--- Ass1.py
+++ Ass1.py
@@ -3,7 +3,7 @@ import pandas as pd
 import igraph
 import matplotlib.pyplot as plt
 
-data = pd.read_excel (r'C:\Users\Thierry\Documents\Studie\TU Delft Applied Physics\CS4195 Modeling and Data Analysis in Complex Networks\Assignment1\manufacturing_emails_temporal_network.xlsx')
+#data = pd.read_excel (r'C:\Users\Thierry\Documents\Studie\TU Delft Applied Physics\CS4195 Modeling and Data Analysis in Complex Networks\Assignment1\manufacturing_emails_temporal_network.xlsx')
 data = pd.read_excel (r'C:\Users\cleoo\Documents\Complex Network\manufacturing_emails_temporal_network.xlsx')
 #%% 1)
 G = data.drop(['timestamp'],axis=1)
@@ -28,26 +28,44 @@ Dvar = np.var(list(g.degree()))
 print(Dvar)
 
 #%% 2)
+from scipy.optimize import curve_fit
+def powerlaw(x, a, k):
+    return a*x**(k)
 
-#hist = np.histogram(g.degree(),np.max(g.degree()))
-plt.hist(g.degree(),bins=np.max(g.degree()),weights=np.ones(Nnodes)/Nnodes)
-plt.xlabel('Degree')
-plt.ylabel('Pr[D=k]')
-plt.title('Degree distribution')
-plt.show()
+bin_heights, bin_borders, _ = plt.hist(g.degree(),bins=np.max(g.degree()),weights=np.ones(Nnodes)/Nnodes, histtype = 'step', log=False, label='data')
+bin_centers = bin_borders[:-1] + np.diff(bin_borders) / 2
+centers = np.take(bin_centers, np.nonzero(bin_heights))
+centers = centers.reshape((65,))
+heights = np.take(bin_heights, np.nonzero(bin_heights))
+heights = heights.reshape((65,))
+popt, _ = curve_fit(powerlaw, centers, heights, p0=[10**(-1), -3.])
+
+x_interval_for_fit = np.linspace(bin_borders[0], bin_borders[-1], 10000)
+plt.plot(x_interval_for_fit, powerlaw(x_interval_for_fit, *popt), label='fit')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('Log(Degree)')
+plt.ylabel('Log(Pr[D=k])')
+plt.legend()
 
 #%% 3)
 
 rhoD = g.assortativity_degree()
 
-#%% 4)
+#%% 4) 6)
+#Generate an ER random graph to investigate small world property
+ER_graph = igraph.Graph()
+ER_graph = ER_graph.Erdos_Renyi(Nnodes,m=Nlinks,directed=False, loops=False)
 
 C = g.transitivity_undirected()
+CER = ER_graph.transitivity_undirected()
 
-#%% 5)
+#%% 5) 6)
 H = g.shortest_paths()
-EH = np.sum(np.sum(H,axis=1),axis=0)/((Nnodes-1)*Nnodes)
+EH = np.sum(np.sum(H,axis=1),axis=0)/((Nnodes-1)*Nnodes) #EH = APL
+
 APL = g.average_path_length()
+APL_ER = ER_graph.average_path_length() #compare with random graph
 diam = np.max(H)
 
 #%% 7)
@@ -55,9 +73,11 @@ diam = np.max(H)
 Adj = g.get_adjacency()
 Adj = np.array([Adj.data])
 Eig = np.linalg.eig(Adj)
-MaxEig = np.max(Eig)
+MaxEig = np.max(Eig[0])
 
 #%% 8)
 Lap = g.laplacian()
 Lap = np.array([Lap])
-Eiglap = np.linalg.eig(Lap)
+Eiglap = np.linalg.eig(Lap) #stores eigenvalues and eigenvectors
+Alg_con = np.sort(Eiglap[0])
+Alg_con = Alg_con[:,1] #take second smallest eigenvalue
